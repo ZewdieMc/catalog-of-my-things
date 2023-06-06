fdce81995f67350ca79883e49c2006bb13fb8ca6
Zewdie Habtie <zewdiehabtie26@gmail.com>
2023-06-06 21:08:42 +0000

    Create game class implement methods

---
+++ game.rb
@@ -0,0 +1,16 @@
+require './item'
+require 'date'
+
+class Game < Item
+
+    def initialize(publish_date, archived, multiplayer, last_played_at)
+        super(publish_date, archived)
+        @multiplayer = multiplayer
+        @last_played_at = last_played_at
+    end
+
+    private
+    def can_be_archived?
+        super && Date.today.year - @last_played_at.year > 2
+    end
+end

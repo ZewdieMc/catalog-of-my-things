65dea298a9eb276e18d271deba42c3d70ce944b1
byiringiroscar <byiringoroscar@gmail.com>
2023-06-06 11:08:18 +0200

    Add main

--- item.rb
+++ item.rb
@@ -24,3 +24,5 @@ class Item
     Date.today.year - @publish_date.year >= 10
   end
 end
+
+
--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
+require_relative 'item'
+
 # main.rb file code goes here
+
+
+class Main
+    def self.run
+        item = Item.new("12/15/2000", false)
+        puts item.publish_date
+    end
+end
+
+Main.run

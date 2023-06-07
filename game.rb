2b3f039945055682744e62d242b9b13566faa59e
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-07 09:57:11 +0300

    Fix linter errors

--- game.rb
+++ game.rb
@@ -2,15 +2,18 @@ require './item'
 require 'date'
 
 class Game < Item
+  def initialize(publish_date, archived, multiplayer, last_played_at)
+    super(publish_date, archived)
+    @multiplayer = multiplayer
+    @last_played_at = last_played_at
+  end
 
-    def initialize(publish_date, archived, multiplayer, last_played_at)
-        super(publish_date, archived)
-        @multiplayer = multiplayer
-        @last_played_at = last_played_at
-    end
+  private
 
-    private
-    def can_be_archived?
-        super && Date.today.year - @last_played_at.year > 2
-    end
+  def can_be_archived?
+    super && Date.today.year - @last_played_at.year > 2
+  end
 end
+
+game = Game.new('12/15/2000', false, true, '12/15/2023')
+puts game.can_be_archived?

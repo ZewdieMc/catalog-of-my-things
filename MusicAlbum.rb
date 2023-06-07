156b41d965cec7534aff68f68ce075bc71146052
byiringiroscar <byiringoroscar@gmail.com>
2023-06-07 07:40:42 +0200

    Add class genre, album music and their methods

---
+++ MusicAlbum.rb
@@ -0,0 +1,10 @@
+class Musicalbum
+    attr_accessor :on_spotify
+    def initialize(on_spotify)
+        @on_spotify = on_spotify
+    end
+
+    def can_be_archived?
+        super && @on_spotify
+    end
+end
---
+++ genre.rb
@@ -0,0 +1,15 @@
+require_relative 'item'
+
+class Genre
+    attr_reader :id
+    attr_accessor :name
+    def initialize(name)
+        @id = Random.rand(1..1000)
+        @name = name
+        @items = []
+    end
+    def add_item(item)
+        @items << item
+        item.genre = self
+    end
+end

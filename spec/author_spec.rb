057f30c0959e7af358376412b670b587ac352eae
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-07 15:38:50 +0300

    Fix linter errors

--- spec/author_spec.rb
+++ spec/author_spec.rb
@@ -3,14 +3,14 @@ require './game'
 require './item'
 
 describe Author do
-    subject { described_class.new('Cal', 'Newport') }
-    describe '#add_item' do
-        it 'should add an item to the items array' do
-            game = Game.new(Date.new(2000, 1, 1), false, true, Date.new(2018, 1, 1))
-            subject.add_item(game)
-            expect(subject.items).to include(game)
-            expect(game.author).to eq(subject)
-            expect(game.author.first_name).to eq('Cal')
-        end
+  subject { described_class.new('Cal', 'Newport') }
+  describe '#add_item' do
+    it 'should add an item to the items array' do
+      game = Game.new(Date.new(2000, 1, 1), false, true, Date.new(2018, 1, 1))
+      subject.add_item(game)
+      expect(subject.items).to include(game)
+      expect(game.author).to eq(subject)
+      expect(game.author.first_name).to eq('Cal')
     end
+  end
 end

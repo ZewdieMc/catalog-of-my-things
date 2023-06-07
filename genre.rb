90faa6bbe20212d079426f1c0c3aae953b964e46
byiringiroscar <byiringoroscar@gmail.com>
2023-06-07 11:54:49 +0200

    Fix linters

--- genre.rb
+++ genre.rb
@@ -1,15 +1,17 @@
 require_relative 'item'
 
 class Genre
-    attr_reader :id
-    attr_accessor :name, :items
-    def initialize(name)
-        @id = Random.rand(1..1000)
-        @name = name
-        @items = []
-    end
-    def add_item(item)
-        @items << item
-        item.genre = self
-    end
+  attr_reader :id
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @id = Random.rand(1..1000)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    @items << item
+    item.genre = self
+  end
 end

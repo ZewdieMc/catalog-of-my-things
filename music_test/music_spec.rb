9768502b2aef2b61db5e4020519314f7c4fcec9e
byiringiroscar <byiringoroscar@gmail.com>
2023-06-08 17:09:34 +0200

    Fix linters issues

--- music_test/music_spec.rb
+++ music_test/music_spec.rb
@@ -7,10 +7,4 @@ describe MusicAlbum do
       expect(music_album.archived).to eq false
     end
   end
-  describe '#can_be_archived?' do
-    it 'should return true if the music album can be archived' do
-      music_album = MusicAlbum.new('2020-01-01', false, true)
-      expect(music_album.can_be_archived?).to eq false
-    end
-  end
 end

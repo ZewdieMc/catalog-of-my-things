f46edbe5f592a0c029f32b708eeda18e3f754a46
Besufekad Alemu <besurye@gmail.com>
2023-06-08 21:15:04 +0300

    Create table author,label and item in schema.sql file

--- schema/schema.sql
+++ schema/schema.sql
@@ -7,4 +7,32 @@ CREATE TABLE genre (
   name VARCHAR(100) NOT NULL
 );
 
+-- Create author table
+CREATE TABLE author (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL
+);
+
+-- Create label table
+CREATE TABLE label (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  title VARCHAR(100) NOT NULL,
+  color VARCHAR(100) NOT NULL,
+  item_ids INT[] NOT NULL
+);
+
+-- Create item table
+CREATE TABLE item (
+  id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  genre_id INT NOT NULL,
+  author_id INT NOT NULL,
+  label_id INT NOT NULL,
+  published_at DATE NOT NULL,
+  is_archived BOOLEAN NOT NULL,
+  FOREIGN KEY (genre_id) REFERENCES genre (id),
+  FOREIGN KEY (author_id) REFERENCES author (id),
+  FOREIGN KEY (label_id) REFERENCES label (id)
+);
+
 -- Create book table (specialized form of item)

61196f8144071f98289fb75548cb45194d6346be
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-08 17:30:51 +0300

    Add constructor to Main class

--- main.rb
+++ main.rb
@@ -6,12 +6,17 @@ require_relative 'choice'
 require_relative './books/book_ui'
 
 class Main
+  def initialize(book_app = BookActions.new, game = GameOptions.new)
+    @book_app = book_app
+    @game = game
+    @choice = Choice.new(@book_app, @game)
+  end
   def run
     puts "Welcome to our Catalog of things \n"
     puts "Please choose an option by entering a number!\n"
     loop do
       Options.new
-      Choice.new
+      @choice.run
     end
   end
 end

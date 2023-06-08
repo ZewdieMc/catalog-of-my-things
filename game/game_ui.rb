a3f423f88d82e9e645c700e56636c057038192a8
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-08 17:30:25 +0300

    Replace double quote with single

--- game/game_ui.rb
+++ game/game_ui.rb
@@ -4,9 +4,11 @@ require_relative '../author'
 class GameOptions
   attr_accessor :games, :authors
 
-  @@games = []
-  @@authors = []
-
+  def initialize
+    @games = []
+    @authors = []
+  end
+    
   def add_game
     print 'Enter the publish date of the game (YYYY-MM-DD): '
     publish_date = Date.parse(gets.chomp)
@@ -15,36 +17,36 @@ class GameOptions
     multiplayer = %w[N n NO no No].include? multiplayer_input ? false : true
     last_played_at = Date.today
     game = Game.new(publish_date, multiplayer, last_played_at)
-    print "Enter game author First Name: "
+    print 'Enter game author First Name: '
     first_name = gets.chomp
-    print "Enter game author Last Name: "
+    print 'Enter game author Last Name: '
     last_name = gets.chomp
     author = Author.new(first_name, last_name)
     author.add_item(game)
-    @@games.push(game)
-    puts "Game created successfully."
+    @games.push(game)
+    puts 'Game created successfully.'
   end
 
   def list_games
-    puts 'There are no games in the library' if @@games.empty?
-    @@games.each do |game|
-      puts game.label === nil ? "Label: No label" : "Label: #{game.label.title}"
+    puts 'There are no games in the library' if @games.empty?
+    @games.each do |game|
+      puts game.label.nil? ? 'Label: No label' : "Label: #{game.label.title}"
       puts "Publish date: #{game.publish_date}"
-      puts game.multiplayer === true ? "Player: multiplayer" : "Player: singleplayer"
+      puts game.multiplayer == true ? 'Player: multiplayer' : 'Player: singleplayer'
       puts "Last played at: #{game.last_played_at}"
-      puts game.author === nil ? "Author: No author" : "Author: #{game.author.first_name} #{game.author.last_name}"
-      puts game.genre === nil ? "Genre: No genre" : "Genre: #{game.genre.name}"
-      puts "============================"
+      puts game.author.nil? ? 'Author: No author' : "Author: #{game.author.first_name} #{game.author.last_name}"
+      puts game.genre.nil? ? 'Genre: No genre' : "Genre: #{game.genre.name}"
+      puts '============================'
     end
     puts
   end
 
   def list_authors
-    @@authors = @@games.map(&:author).uniq
-    puts 'There are no authors in the library' if @@authors.empty?
-    @@authors.each.with_index do |author, index|
-      puts "Author #{index +1}: #{author.first_name} #{author.last_name}"
-      puts "============================"
+    @authors = @games.map(&:author).uniq
+    puts 'There are no authors in the library' if @authors.empty?
+    @authors.each.with_index do |author, index|
+      puts "Author #{index + 1}: #{author.first_name} #{author.last_name}"
+      puts '============================'
     end
   end
 end

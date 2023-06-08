dcf8b7a4c5cb12db89e39afb1a1374e382108ea0
Besufekad Alemu <besurye@gmail.com>
2023-06-08 16:35:59 +0300

    Finalize all tests and fix rubocop linter erros

--- spec/list_labels_spec.rb
+++ spec/list_labels_spec.rb
@@ -14,13 +14,7 @@ RSpec.describe BookActions do
           "publish_date": "2023-06-07",
           "publisher": "publisher one",
           "cover_state": "good",
-          "labels": [
-            {
-              "id": 1,
-              "name": "gift",
-              "color": "red"
-            }
-          ]
+          "labels": [{ "id": 1, "name": "gift", "color": "red" }]
         },
         {
           "title": "Test Book 2",
@@ -28,32 +22,25 @@ RSpec.describe BookActions do
           "publish_date": "2023-06-08",
           "publisher": "publisher two",
           "cover_state": "bad",
-          "labels": [
-            {
-              "id": 2,
-              "name": "buy",
-              "color": "blue"
-            }
-          ]
+          "labels": [{ "id": 2, "name": "buy", "color": "blue" }]
         }
       ]
     JSON
+
     allow(File).to receive(:read).with('./json_data/books.json').and_return(@mock_json_data)
+
+    @label1 = Label.new(1, 'gift', 'red')
+    @label2 = Label.new(2, 'buy', 'blue')
+    @book1 = Book.new('Test author 1', Date.new(2023, 6, 7), 'publisher one', 'good')
+    @book2 = Book.new('Test author 2', Date.new(2023, 6, 8), 'publisher two', 'bad')
+    @book1.label = @label1
+    @book2.label = @label2
     @book_app = BookActions.new(file: './json_data/books.json')
+    @book_app.books = [@book1, @book2]
   end
 
   describe '#list_labels' do
     context 'when there are labels in the list' do
-      before do
-        @label1 = Label.new(1, 'gift', 'red')
-        @label2 = Label.new(2, 'buy', 'blue')
-        @book1 = Book.new('Test author 1', Date.new(2023, 6, 7), 'publisher one', 'good')
-        @book1.label = @label1
-        @book2 = Book.new('Test author 2', Date.new(2023, 6, 8), 'publisher two', 'bad')
-        @book2.label = @label2
-        @book_app.books = [@book1, @book2]
-      end
-
       it 'prints the label information' do
         expect { @book_app.list_labels }.to output(/Color: red - Name: gift/).to_stdout
         expect { @book_app.list_labels }.to output(/Color: blue - Name: buy/).to_stdout
@@ -61,9 +48,7 @@ RSpec.describe BookActions do
     end
 
     context 'when there are no labels in the list' do
-      before do
-        @book_app.books = []
-      end
+      before { @book_app.books = [] }
 
       it 'prints a message indicating no labels found' do
         expect { @book_app.list_labels }.to output(/No labels found!/).to_stdout
@@ -71,4 +56,3 @@ RSpec.describe BookActions do
     end
   end
 end
-

492bda779a089d7fd6429f1c26823b14ac893452
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-09 15:19:55 +0300

    Fix linter error

--- game/game_ui.rb
+++ game/game_ui.rb
@@ -121,7 +121,7 @@ class GameOptions
       last_played_at = game_data[:last_played_at]
       game = Game.new(publish_date, multiplayer, last_played_at)
       game.label = game_data[:label]
-      author = Author.new(game_data[:author].split(' ')[0], game_data[:author].split(' ')[1])
+      author = Author.new(game_data[:author].split[0], game_data[:author].split[1])
       game.author = author
       game.genre = game_data[:genre]
       @games.push(game)

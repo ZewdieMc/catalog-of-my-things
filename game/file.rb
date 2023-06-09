f164108b5b9a2cac08c87710962f3fe486015045
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-09 14:15:35 +0300

    Implement Preservation

---
+++ game/file.rb
@@ -0,0 +1,7 @@
+value = false
+author = if value == true
+           'Zewdie'
+         else
+           'Genet'
+         end
+puts author
--- game/game_ui.rb
+++ game/game_ui.rb
@@ -1,15 +1,15 @@
 require_relative '../game'
 require_relative '../author'
-require 'fileutils'
 
 class GameOptions
-  FILE_PATH = "#{__dir__}/../json_data/games.json".freeze
+  GAMES_FILE = "#{__dir__}/../json_data/games.json".freeze
+  AUTHORS_FILE = "#{__dir__}/../json_data/authors.json".freeze
 
   attr_accessor :games, :authors
 
   def initialize
-    @games = []
-    @authors = []
+    @games = load_games || []
+    @authors = load_authors || []
   end
 
   def add_game
@@ -20,18 +20,39 @@ class GameOptions
     multiplayer = %w[N n NO no No].include? multiplayer_input ? false : true
     last_played_at = Date.today
     game = Game.new(publish_date, multiplayer, last_played_at)
-    print 'Enter game author First Name: '
-    first_name = gets.chomp
-    print 'Enter game author Last Name: '
-    last_name = gets.chomp
-    author = Author.new(first_name, last_name)
+    author = choose_author
     author.add_item(game)
-    @games.push(game)
+    store_authors
+    @games<<game
     puts 'Game created successfully.'
+    store_games
+  end
+
+  def choose_author
+    author = nil
+    puts 'Do you want to add a new author or choose from available authors? [N/L]? '
+    puts 'N: New author'
+    puts 'L: List authors' unless authors.empty?
+    input = gets.chomp.upcase
+    if input == 'N'
+      print 'Enter game author First Name: '
+      first_name = gets.chomp
+      print 'Enter game author Last Name: '
+      last_name = gets.chomp
+      author = Author.new(first_name, last_name)
+      @authors.push(author)
+    elsif input == 'L'
+      puts 'Select an author from the list below:'
+      @authors.each.with_index { |auth, index| puts "#{index}): #{auth.first_name} #{auth.last_name}" }
+      author_index = gets.chomp.to_i
+      author = @authors[author_index]
+    end
+    author
   end
 
   def list_games
     puts 'There are no games in the library' if @games.empty?
+    load_games
     @games.each do |game|
       puts game.label.nil? ? 'Label: No label' : "Label: #{game.label.title}"
       puts "Publish date: #{game.publish_date}"
@@ -53,11 +74,77 @@ class GameOptions
     end
   end
 
+  def store_games
+    games_arr = []
+    @games.each do |game|
+      puts game.author
+      games_arr.push({
+                       id: game.id,
+                       publish_date: game.publish_date,
+                       multiplayer: game.multiplayer,
+                       last_played_at: game.last_played_at,
+                       label: game.label,
+                       author: game.author.nil? ? nil : "#{game.author.first_name} #{game.author.last_name}"
+                     })
+    end
+    File.write(GAMES_FILE, JSON.pretty_generate(games_arr))
+  end
+
+  def store_authors
+    authors_arr = []
+    @authors.each do |author|
+      authors_arr.push({
+                         first_name: author.first_name,
+                         last_name: author.last_name,
+                         items: author.items.map do |item|
+                                  {
+                                    id: item.id,
+                                    publish_date: item.publish_date,
+                                    archived: item.archived
+                                  }
+                                end
+                       })
+    end
+    File.write(AUTHORS_FILE, JSON.pretty_generate(authors_arr))
+  end
+
   def load_games
-    return [] if !File.exist?(FILE_PATH) || File.empty(FILE_PATH)
-    json_data = File.read(FILE_PATH)
-    games_data = JSON.parse(json_data, symbolize_names: true)
+    return [] if !File.exist?(GAMES_FILE) || File.empty?(GAMES_FILE)
+
+    games_data = JSON.parse(File.read(GAMES_FILE), symbolize_names: true)
     return [] unless games_data.is_a?(Array)
-    
+
+    @games = []
+    games_data.each do |game_data|
+      publish_date = game_data[:publish_date]
+      multiplayer = game_data[:multiplayer]
+      last_played_at = game_data[:last_played_at]
+      game = Game.new(publish_date, multiplayer, last_played_at)
+      game.label = game_data[:label]
+      author = Author.new(game_data[:author]['first_name'], game_data[:author]['last_name'])
+      game.author = author
+      game.genre = game_data[:genre]
+      @games.push(game)
+    end
+    @games
+  end
+
+  def load_authors
+    return [] if !File.exist?(AUTHORS_FILE) || File.empty?(AUTHORS_FILE)
+
+    author_data = JSON.parse(File.read(AUTHORS_FILE), symbolize_names: true)
+    return [] unless author_data.is_a?(Array)
+
+    @authors = []
+    author_data.each do |author_d|
+      first_name = author_d[:first_name]
+      last_name = author_d[:last_name]
+      author = Author.new(first_name, last_name)
+      author_d[:items].map do |item|
+        author.add_item(Item.new(item[:publish_date], item[:archived], item[:id]))
+      end
+      @authors.push(author)
+    end
+    @authors
   end
 end
